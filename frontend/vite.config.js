c1b5833ecef9cf989d5cf07a5f45cda780d65246
cacamaniya <shlya12332q1@yandex.ru>
2025-07-28 01:24:43 +0500

    Container for frontend, dif env var for local and docker proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const backendHost = process.env.VITE_API_BASE_URL || 'http://localhost:8080'
+console.log('Backend URL:', process.env.VITE_API_BASE_URL)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
     server: {
+    host: true, 
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // target: 'http://localhost:8080', for local
+        target: backendHost,
         changeOrigin: true,
         secure: false,
       }
