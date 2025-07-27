da40aea14492ba5809815ed7c48a115840002040
cacamaniya <shlya12332q1@yandex.ru>
2025-07-27 23:13:48 +0500

    .env for local usage, proxy for front dev

--- db/db.go
+++ db/db.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -21,14 +22,21 @@ func Init() {
 	local := flag.Bool("local", false, "Локальное подключение")
 	flag.BoolVar(local, "l", false, "Синоним --local")
 	flag.Parse()
-
+	godotenv.Load()
 	var dsn string
 	if *local {
-		dsn = "host=localhost user=postgres password=1111 dbname=go_demo port=5432 sslmode=disable"
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			os.Getenv("DB_HOST_LOCAL"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
 	} else {
 		dsn = fmt.Sprintf(
 			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-			os.Getenv("DB_HOST"),
+			os.Getenv("DB_HOST_DOCKER"),
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_NAME"),
@@ -36,6 +44,7 @@ func Init() {
 		)
 	}
 
+	fmt.Println(dsn)
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
